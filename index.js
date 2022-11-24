d116e3a30900652a8f8242959850e77692c0444b
NicolasFontana <nico.fontana12@gmail.com>
2022-11-24 12:41:06 -0300

    finished project

--- index.js
+++ index.js
@@ -4,11 +4,13 @@
 // init project
 var express = require('express');
 var app = express();
+require('dotenv').config()
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+const PORT = process.env.PORT || 8000
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -19,14 +21,30 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+// your first API endpoint...
+app.get("/api", function (req, res) {
+  const dateNow = new Date()
+  res.json({unix: dateNow.getTime(), utc: dateNow.toGMTString()})
 });
 
-
+app.get("/api/:date", (req, res) => {
+  const timestamp = req.params.date
+  if (/\d{5,}/.test(timestamp)) {
+    const date = new Date(Number(timestamp))
+    if(date == "Invalid Date") {
+      res.json({ error: "Invalid Date" })
+    }
+    res.json({unix: Number(timestamp), utc: date.toGMTString()})
+  } else {
+    const date = new Date(timestamp)
+    if(date == "Invalid Date") {
+      res.json({ error: "Invalid Date" })
+    }
+    res.json({unix: date.getTime(), utc: date.toGMTString()})
+  }
+})
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+var listener = app.listen(PORT, function () {
+  console.log('Your app is listening on port ' + PORT);
 });
